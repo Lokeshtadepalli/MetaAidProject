b0e9ede3c8f5d9a6ff8aa942374823a80cde2086
Lokeshtadepalli <lokeshtadepalli2002@gmail.com>
2024-01-31 13:32:39 +0530

    commit

---
+++ src/java/com/spring/Model/College.java
@@ -0,0 +1,40 @@
+package com.spring.Model;
+
+
+
+
+public class College 
+{
+	private Integer id;
+	private String name;
+    private Integer collegeid;
+	
+	
+
+	public Integer getCollegeid() {
+		return collegeid;
+	}
+
+	public void setCollegeid(Integer collegeid) {
+		this.collegeid = collegeid;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+}
---
+++ src/java/com/spring/Model/District.java
@@ -0,0 +1,28 @@
+package com.spring.Model;
+
+public class District {
+	
+	private Integer districtid;
+	private String districtname;
+	
+	
+	public Integer getDistrictid() {
+		return districtid;
+	}
+	public void setDistrictid(Integer districtid) {
+		this.districtid = districtid;
+	}
+	public String getDistrictname() {
+		return districtname;
+	}
+	public void setDistrictname(String districtname) {
+		this.districtname = districtname;
+	}
+
+	
+	
+	
+	
+	
+
+}
